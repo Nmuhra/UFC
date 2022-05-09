7675db25c7c6d3e27bf244b6a3cab48eccc1afd2
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-05-09 17:49:10 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+rankings = "https://www.ufc.com/rankings"
+print("live rankings:\n"+rankings)
+name = input("UFC fighter's name:\n")
+surname = input("UFC fighter's surname:\n")
+link = f"https://www.ufc.com/athlete/{name}-{surname}"
+print(link)
