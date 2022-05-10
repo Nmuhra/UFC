7e49e10319ae981f0f3a0f7837af93b269901cfb
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-05-10 21:54:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ name = input("UFC fighter's name:\n")
 surname = input("UFC fighter's surname:\n")
 link = f"https://www.ufc.com/athlete/{name}-{surname}"
 print(link)
+events = "https://www.ufc.com/events"
+print("next event:\n"+events)
